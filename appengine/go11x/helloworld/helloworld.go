a1933f69595b884969cfc9ab57b4e34248739802
bob <bob.boule@gmail.com>
2020-12-22 17:24:00 -0500

    1723 test

--- appengine/go11x/helloworld/helloworld.go
+++ appengine/go11x/helloworld/helloworld.go
@@ -55,7 +55,7 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
 		http.NotFound(w, r)
 		return
 	}
-	fmt.Fprint(w, "Hello, World! 12/22/2020 17:15")
+	fmt.Fprint(w, "Hello, World! 12/22/2020 17:23")
 }
 
 // [END indexHandler]
